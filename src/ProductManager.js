f5a49770d3e1a1e127113ebc0105af0d983b6757
mariano gaudi <marianocamerlogaudi@gmail.com>
2023-09-26 22:55:01 -0300

    preentrega_3

---
+++ src/ProductManager.js
@@ -0,0 +1,97 @@
+import fs from "fs/promises";
+
+class ProductManager {
+  constructor(filePath) {
+    this.path = filePath;
+  }
+
+  async loadProductsFromFile() {
+    try {
+      const data = await fs.readFile(this.path, "utf8");
+      return JSON.parse(data);
+    } catch (error) {
+      return [];
+    }
+  }
+
+  async saveProductsToFile(products) {
+    try {
+      await fs.writeFile(this.path, JSON.stringify(products, null, 2), "utf8");
+    } catch (error) {
+      console.error("Error al guardar productos en el archivo:", error);
+    }
+  }
+
+  async addProduct(product) {
+    const products = await this.loadProductsFromFile();
+    if (
+      !product.title ||
+      !product.description ||
+      !product.price ||
+      !product.thumbnail ||
+      !product.stock
+    ) {
+      console.error("Todos los campos son obligatorios.");
+      return;
+    }
+
+    product.id = this.getNextId(products);
+
+    products.push(product);
+    await this.saveProductsToFile(products);
+  }
+
+  getNextId() {
+    const lastId =
+      this.products.length > 0 ? this.products[this.products.length - 1].id : 0;
+    return lastId + 1;
+  }
+
+  async getProducts() {
+    return await this.loadProductsFromFile();
+  }
+
+  async getProductById(id) {
+    const products = await this.loadProductsFromFile();
+    const product = products.find((product) => product.id === id);
+    if (!product) {
+      console.log(`El producto con ID ${id} no existe.`);
+    }
+    return product;
+  }
+
+  async updateProduct(id, updatedProduct) {
+    const products = await this.loadProductsFromFile();
+    const index = products.findIndex((product) => product.id === id);
+
+    if (index === -1) {
+      console.error(`Producto con ID ${id} no encontrado.`);
+      return false;
+    }
+
+    products[index] = {
+      ...products[index],
+      ...updatedProduct,
+    };
+
+    await this.saveProductsToFile(products);
+    return true;
+  }
+
+  async deleteProduct(id) {
+    const products = await this.loadProductsFromFile();
+    const index = products.findIndex((product) => product.id === id);
+
+    if (index === -1) {
+      console.error(`Producto con ID ${id} no encontrado.`);
+      return false;
+    }
+
+    products.splice(index, 1);
+
+    await this.saveProductsToFile(products);
+    return true;
+  }
+}
+
+export default ProductManager;
---
+++ src/app.js
@@ -0,0 +1,48 @@
+import express from "express";
+import path from "path";
+import ProductManager from "./ProductManager.js";
+
+const port = 8080;
+const app = express();
+const server = app.listen(port, () =>
+  console.log(`Servidor activo en puerto ${port}`)
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const absolutePath = path.join(process.cwd(), "src", "productos.json");
+const productManager = new ProductManager(absolutePath);
+
+app.get("/products", async (req, res) => {
+  const { limit } = req.query;
+  try {
+    const products = await productManager.loadProductsFromFile();
+    if (limit) {
+      const limitedProducts = products.slice(0, parseInt(limit));
+      res.json(limitedProducts);
+    } else {
+      res.json(products);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener productos" });
+  }
+});
+
+// Endpoint para obtener un producto por ID
+app.get("/products/:pid", async (req, res) => {
+  const { pid } = req.params;
+  try {
+    const products = await productManager.loadProductsFromFile();
+    const product = products.find((p) => p.id === parseInt(pid));
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ error: `Producto con ID ${pid} no encontrado` });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener el producto" });
+  }
+});
+
+server;
